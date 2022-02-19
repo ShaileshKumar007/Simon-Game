92a2945902f94382d4688c4781aee3f4583c8a30
Shailesh <shailesh1429k@gmail.com>
2022-02-20 02:55:02 +0530

    js to get next sequence

--- game.js
+++ game.js
@@ -40,3 +40,21 @@ function animatePress(currentColour){
         document.getElementsByClassName(currentColour)[0].classList.remove("pressed");
     }, 100);
 }
+
+// to get next sequence
+function nextSequence() {
+
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level "+level);
+  
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+  
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  
+    playSound(randomChosenColour);
+  }
+
+  
